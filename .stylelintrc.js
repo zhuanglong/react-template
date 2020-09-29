7a61f1bd4183040f02af130299ea6c778e4ef69d
zhuanglong <>
2020-09-29 17:41:18 +0800

    stylelint

---
+++ .stylelintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // 继承规则集
+  extends: ['stylelint-config-standard'],
+
+  // 自定义规则
+  rules: {
+    'declaration-empty-line-before': null,
+    'no-descending-specificity': null,
+    'selector-pseudo-class-no-unknown': null,
+    'selector-pseudo-element-colon-notation': null
+  }
+};
