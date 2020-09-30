3f5731ae37e5909e7c4807a5c3e3d2b526422ef8
zhuanglong <>
2020-09-30 23:07:11 +0800

    stylelint-config-rational-order

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
   // 继承规则集
-  extends: ['stylelint-config-standard'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-rational-order'
+  ],
 
   // 自定义规则
   rules: {
