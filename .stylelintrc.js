3f172293533e0898917e6b465c3a519724437297
zhuanglong <>
2020-10-02 09:57:42 +0800

    stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -6,10 +6,5 @@ module.exports = {
   ],
 
   // 自定义规则
-  rules: {
-    'declaration-empty-line-before': null,
-    'no-descending-specificity': null,
-    'selector-pseudo-class-no-unknown': null,
-    'selector-pseudo-element-colon-notation': null
-  }
+  rules: {}
 };
