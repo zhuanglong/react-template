b85d094e2810da459a4b8c5b434c13b9a71beea9
zhuanglong <2766428466@qq.com>
2021-04-09 14:21:09 +0800

    新增 自定义动画

--- src/pages/Animation/index.js
+++ src/pages/Animation/index.js
@@ -6,6 +6,7 @@ import AnimationExm1 from './AnimationExm1';
 import AnimationExm2 from './AnimationExm2';
 import AnimationExm3 from './AnimationExm3';
 import AnimationExm4 from './AnimationExm4';
+import AnimationExm5 from './AnimationExm5';
 
 function Animation() {
   return (
@@ -15,12 +16,14 @@ function Animation() {
         <li><Link to="/animation/animation-exm2">Exm2</Link></li>
         <li><Link to="/animation/animation-exm3">Exm3</Link></li>
         <li><Link to="/animation/animation-exm4">Exm4</Link></li>
+        <li><Link to="/animation/animation-exm5">Exm5</Link></li>
       </ul>
       <div style={{ height: '1px', background: 'blue', margin: '10px 0' }} />
       <Route path="/animation/animation-exm1" component={AnimationExm1} />
       <Route path="/animation/animation-exm2" component={AnimationExm2} />
       <Route path="/animation/animation-exm3" component={AnimationExm3} />
       <Route path="/animation/animation-exm4" component={AnimationExm4} />
+      <Route path="/animation/animation-exm5" component={AnimationExm5} />
     </div>
   );
 }
