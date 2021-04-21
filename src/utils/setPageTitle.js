ae51468fe8183966d082e3bbcd3111f2cb6e8cf1
zhuanglong <2766428466@qq.com>
2021-04-21 10:15:59 +0800

    修复页面标题

--- src/utils/setPageTitle.js
+++ src/utils/setPageTitle.js
@@ -1,10 +1,10 @@
 function setTitle(ev) {
-  const hash = ev.path[0].location.hash.substr(1);
-  if (hash.startsWith('/index/home')) {
+  const path = ev.target.location.hash.substr(1);
+  if (path.startsWith('/index/home')) {
     document.title = '首页';
-  } else if (hash.startsWith('/index/message')) {
+  } else if (path.startsWith('/index/message')) {
     document.title = '消息';
-  } else if (hash.startsWith('/index/my')) {
+  } else if (path.startsWith('/index/my')) {
     document.title = '我的';
   } else {
     document.title = '默认标题';
